df56d581a088b87f99d15fc273df2eee55ab72a8
Moulik Aggarwal <moulik@deepsource.io>
2020-12-03 22:45:07 +0530

    Update for react/button-has-type
    Signed-off-by: Moulik Aggarwal <moulik@deepsource.io>

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-var Hello = <button type={condition ? foo : "button"}/>
+import React from "react";
 
-export default Hello;
+export const Button = React.memo(({ onClick, label, n }) => {
+  let condition = true;
+  return (
+    <button
+      className="btn btn-primary mt-3"
+      onClick={() => onClick(n)}
+      type="foo"
+    >
+      {label}
+    </button>
+  );
+});
